7686cf7233c016293859e74b48fffb005cd1d83e
Lisa-Ho <lisa_hornung@gmx.de>
2023-04-02 17:32:03 +0100

    renamed columns ++ changed emoji ++ changed main colour ++ added sidebar

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-
 import streamlit as st
 
 st.set_page_config(
     page_title="ridgemapp",
-    page_icon="🖼️",
+    page_icon="🗺️",
     initial_sidebar_state="collapsed"
     #layout="wide",
      )
@@ -13,25 +12,34 @@ from folium.plugins import Draw
 from streamlit_folium import st_folium
 from streamlit_image_select import image_select
 import json
-
 import matplotlib.pyplot as plt
-#from ridge_map_local import RidgeMap, FontManager
 from plotting import create_map
 
 #load predefined styles stored in json file
 with open('map_styles.json', 'r') as json_file:
     map_styles = json.load(json_file)
 
+#=== Sidebar
+with st.sidebar:
+    st.header("About")
+    st.markdown("You like maps? You like visualisations? Then ridgemapp could be for you.")
+    st.markdown("Create ridge plots of elevation data, customise and download. Print ready for postcards, posters or anything really - makes for some great gifts too.")
+    st.subheader("Limitations")
+    st.markdown("Data might not be available for some parts of the world. Please be mindful with the area you select, if too large the app might crash.")
+    st.subheader("Credits")
+    st.markdown("This app uses the awesome `ridge-map` library to create visually stunning elevation maps. Topographic data comes from NASA's Shuttle Radar Topography Mission ([SRTM](https://www.earthdata.nasa.gov/sensors/srtm)).")
+    st.markdown("Check out ridge-map's [full documentation](https://pypi.org/project/ridge-map/) for more details. The original code is changed slightly to allow transparent backgrounds.")
+    st.markdown("")
+    st.subheader("Get in touch")
+    st.markdown("I'm on [Github](https://github.com/Lisa-Ho), [Mastodon](https://fosstodon.org/@LisaHornung), [Linkedin](https://www.linkedin.com/in/lisa-maria-hornung/), [Twitter](https://twitter.com/LisaHornung_)")
 
-#========== APP
+    st.markdown("Made by Lisa Hornung")
 
+#==== Main App
 st.title('ridgemapp')
-st.markdown("### Create elevation map designs in no time")
-st.markdown("**Select area > Chose style > Customise > Download > Edit, print or share**")
-st.markdown("""---""") 
-
-#========== Sidebar
-
+st.markdown("### Create stunning elevation maps")
+#st.markdown(">**Select area > Chose style > Customise > Download > Edit, print or share**")
+st.markdown("")
 
 # ========= Make selection
 # Write user instructions
@@ -75,14 +83,10 @@ style_id = image_select(
     return_value ="index",
     key="style_selected"
 )
-
-#st.session_state.previous_style_selected = st.session_state.style_selected
-#st.markdown(st.session_state.style_selected)
-
 #get values for selected style using the returned index
 style_selected = map_styles[captions[style_id]]
 
-# ========== Customise map
+# ========== Customise styles
 style_updated = {}
 font_styles = ['Calibri','Cambria','Candara', 'Courier New', 'DejaVu Sans','Ebrima', 'Gabriola','Impact',
              'Lucida Console','Segoe Print', 'Segoe UI', 'Tahoma','Times New Roman', 'Ubuntu', "Verdana"]
@@ -92,71 +96,67 @@ fig_shapes = ["square", "rectangle"]
 with st.form(key="Create map"):
     style_custom = {}
     st.markdown("Customise style")
-    col1, col2 = st.columns([2,1], gap="medium")
-    with col1:
+    col1a, col1b = st.columns([2,1], gap="medium")
+    with col1a:
         style_custom["title"] = st.text_input("Title (optional)", style_selected["title"])
-    with col2:
+    with col1b:
         style_custom["fig_shape"] = st.selectbox('Shape',options=fig_shapes,index=fig_shapes.index(style_selected["fig_shape"]) )
     with st.expander("Change colours, lines and more"):
         st.markdown("**Background**")
-        col3a, col3b, col3c = st.columns([1,1,2], gap="small")
-        with col3a:
+        col2a, col2b, col2c = st.columns([1,1,2], gap="small")
+        with col2a:
             style_custom["bg_transparent"] = st.checkbox('Transparent',value=False)
-        with col3b:
+        with col2b:
             style_custom["bg_color"] = st.color_picker("Colour", style_selected["bg_color"], key=0)
 
         st.markdown("")
         st.markdown("**Title**")
-        col3,col4,col5,col6 = st.columns([1.2,1.7,1,1], gap="small")
-        with col3:
+        col3a,col3b,col3c,col3d = st.columns([1.2,1.7,1,1], gap="small")
+        with col3a:
             style_custom["title_pos"] = st.selectbox('Position',options=title_positions,index=title_positions.index(style_selected["title_pos"]))
-        with col4:
+        with col3b:
             style_custom["title_font"] = st.selectbox('Font',options=font_styles, index= font_styles.index(style_selected["title_font"] ))
-        with col5:
+        with col3c:
             style_custom["title_fontsize"] = st.slider("Fontsize", min_value=10, max_value=60, value=style_selected["title_fontsize"])
-        with col6:
+        with col3d:
             style_custom["title_color"] = st.color_picker('Colour', style_selected["title_color"], key=1)
-        col6a,col6b,col6c = st.columns([1,1.2,2], gap="small")
-        with col6a:
+        
+        col4a,col4b,col4c = st.columns([1,1.2,2], gap="small")
+        with col4a:
             style_custom["title_bg_color"] = st.color_picker('Background colour', style_selected["title_bg_color"], key=2)
-        with col6b:
+        with col4b:
             style_custom["title_bg_alpha"] = st.slider("Background opacity", min_value=0.0, max_value=1.0, value=style_selected["title_bg_alpha"],
             help="0=transparent, 1=solid")
         
         st.markdown("")
         st.markdown("**Ridge lines**")
-        col7,col8,col9 = st.columns([1,1,2], gap="small")
-        with col7:
+        col5a,col5b,col5c = st.columns([1,1,2], gap="small")
+        with col5a:
             style_custom["num_lines"] = st.slider("Number of lines", min_value=30, max_value=150, value=style_selected["num_lines"])
-        with col8:
+        with col5b:
             style_custom["linewidth"] = st.slider("Linewidth", min_value=1, max_value=10, value=style_selected["linewidth"])
-        with col9:
+        with col5c:
             style_custom["line_color"] = st.color_picker('Colour', style_selected["line_color"], key=3)
-        col10,col11,col12,col13 = st.columns([1,1,1,1], gap="small")
-        with col10:
+        col6a,col6b,col6c,col6d = st.columns([1,1,1,1], gap="small")
+        with col6a:
             style_custom["elevation_pts"] = st.slider("Elevation points", min_value=10, max_value=200, value=style_selected["elevation_pts"], help="The more points, the smoother the ridge lines")
-        with col11:
+        with col6b:
             style_custom["vertical_ratio"] = st.slider("Vertical ratio", min_value=10, max_value=150, value=style_selected["vertical_ratio"], help="How steep or flat hills are displayed")
-        with col12:
+        with col6c:
             style_custom["water_ntile"] = st.slider("Water ntile", min_value=0, max_value=8, value=style_selected["water_ntile"], help="Set to 0 if you do not want any water marked")
-        with col13:
+        with col6d:
             style_custom["lake_flatness"] = st.slider("Lake flatness", min_value=0, max_value=8, value=style_selected["lake_flatness"], help="Set to 0 if you do not want any water marked")
 
     button_update_map = st.form_submit_button('Update')
 st.markdown('Your ridgemap design! :point_down: :tada:')
 
 # ========== Manage session states
-#st.markdown("Button: " + str(button_update_map))
-#map updates
 if "update_map" not in st.session_state:
     st.session_state.update_map = False
-#if (st.session_state.style_selected==True) & (button_update_map==False):
-  # st.session_state.update_map = False
 if button_update_map:
     st.session_state.update_map = True
 
-#st.markdown("Session state: " + str(st.session_state.update_map))
-
+# ========== Display map
 # Create map when area selected and update with custom map styles
 if (st.session_state.update_map == False) & (map_selection["last_active_drawing"]!= None):
     style = style_selected
@@ -172,6 +172,7 @@ elif (st.session_state.update_map) & (map_selection["last_active_drawing"]== Non
 else:
     fig = "None" 
 
+# ========== Download
 #export image
 if fig != "None":
     plt.savefig("ridgemaps.png", bbox_inches="tight", dpi=300, pad_inches=0, transparent=style["bg_transparent"])
